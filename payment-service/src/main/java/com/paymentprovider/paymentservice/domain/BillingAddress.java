a919fb794f47f8a73b74525c686f1c3ec18b7e35
Eomaxl <souravsnigdh@gmail.com>
2025-08-27 12:13:21 +0530

    Implemented model class for billing address

--- payment-service/src/main/java/com/paymentprovider/paymentservice/domain/BillingAddress.java
+++ payment-service/src/main/java/com/paymentprovider/paymentservice/domain/BillingAddress.java
@@ -1,2 +1,84 @@
-package com.paymentprovider.paymentservice.domain;public class BillingAddress {
+package com.paymentprovider.paymentservice.domain;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+/**
+ * Billing address information for payment processing
+ * */
+public class BillingAddress {
+
+    @Size(max=255, message = "Street address must not exceed 255 characters")
+    private String street;
+
+    @Size(max = 100, message = "City must not exceed 100 characters")
+    private String city;
+
+    @Size(max = 100, message = "State/Province must not exceed 100 characters")
+    private String state;
+
+    @Size(max = 10, message = "Postal code must not exceed 10 characters")
+    private String postalCode;
+
+    @NotBlank(message = "Country code is required")
+    @Size(min = 2, max = 3, message = "Country code must be 2-3 characters")
+    @Pattern(regexp = "[A-Z]{2,3}$" , message = "Country code must be uppercase letters")
+    private String countryCode;
+
+    public BillingAddress() {}
+
+    public BillingAddress(String street, String city, String state, String postalCode, String countryCode) {
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.postalCode = postalCode;
+        this.countryCode = countryCode;
+    }
+
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    @Override
+    public String toString() {
+        return "BillingAddress [street=" + street + ", city=" + city + ", state=" + state+", postalCode=" + postalCode + ", country=" + countryCode + "]";
+    }
 }
--- payment-service/src/main/java/com/paymentprovider/paymentservice/domain/BillingAdressData.java
+++ payment-service/src/main/java/com/paymentprovider/paymentservice/domain/BillingAdressData.java
@@ -1,2 +1,80 @@
-package com.paymentprovider.paymentservice.domain;public class BillingAdressData {
+package com.paymentprovider.paymentservice.domain;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class BillingAdressData {
+    private String street;
+    private String city;
+    private String state;
+    private String postalCode;
+    private String countryCode;
+
+    public BillingAdressData() {}
+
+    public BillingAdressData(String street, String city, String state, String postalCode, String countryCode) {
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.postalCode = postalCode;
+        this.countryCode = countryCode;
+    }
+
+    public static BillingAdressData from(BillingAddress billingAddress){
+        if (billingAddress == null){
+            return null;
+        }
+        return new BillingAdressData(
+                billingAddress.getStreet(),
+                billingAddress.getCity(),
+                billingAddress.getState(),
+                billingAddress.getPostalCode(),
+                billingAddress.getCountryCode()
+        );
+    }
+
+
+    public BillingAddress toBillingAddress(){
+        return new BillingAddress(street, city, state, postalCode, countryCode);
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
 }
